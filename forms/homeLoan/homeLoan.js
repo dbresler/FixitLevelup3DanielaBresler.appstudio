88990f10b5fd0613d66e09fc473b0d43cc580059
Daniela Bresler <dbresler@github.com>
2020-10-04 16:59:13 -0500

    updates

---
+++ forms/homeLoan/homeLoan.js
@@ -0,0 +1,30 @@
+/*
+btnCal.onclick=function(){
+  
+}
+
+function homeLoanPayment(p, r, n) {
+    let monthlyPayment = p * (r*((1+r)**n))/(((1+r)**n)-1)
+    return monthlyPayment 
+}
+
+  let homeLoanPrice = Number(prompt("Enter your home loan price."))
+  
+  let homeInterestRate = Number(prompt("Enter your monthly interest."))
+  
+  let homeNumberMonths =  Number(prompt("Enter the number of months you're paying."))
+  
+ let homeCalculatePayment = homeLoanPayment(homeLoanPrice, homeInterestRate, homeNumberMonths)
+ 
+ let homeFinalCalculatePayment = parseInt(homeCalculatePayment)
+ 
+ let yearlyHomeMonths = homeNumberMonths / 12
+ 
+console.log(`A home loan for ${homeLoanPrice} over ${yearlyHomeMonths} years at ${homeInterestRate} interest would have a monthly payment of $ ${homeFinalCalculatePayment}`)
+
+
+
+btnCal.onclick=function(){
+  
+}
+*/
